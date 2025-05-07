f1b121fb781962d5230439a1a3c040d048ab7049
Igor Lima Kock <limakock@gmail.com>
2025-05-07 18:22:20 -0300

    Update run.py

--- run.py
+++ run.py
@@ -6,5 +6,4 @@ if __name__ == '__main__':
     with app.app_context():
         from app.models import db
         db.create_all()  # Garante que as tabelas serão criadas
-    'app.run(debug=True)'
-    app.run(debug=False)
+    app.run(debug=True)
